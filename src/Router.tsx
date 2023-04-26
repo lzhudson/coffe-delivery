7e761b2bf930017b08930b9c7e63e56dee16fd2d
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-26 20:57:45 -0300

    feat: create Router component

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import {
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Checkout } from "./pages/Checkout";
+import { Success } from "./pages/Success";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  )
+}
