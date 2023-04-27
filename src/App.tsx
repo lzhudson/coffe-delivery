58b7fdfc7125833de46f1161b67c3854f3beec81
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-26 21:33:08 -0300

    feat: usage ThemeProvider to share defaultTheme variable with all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
-
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
