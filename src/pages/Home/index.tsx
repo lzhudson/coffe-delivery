b5510eb3e9aaa4c1a6aab094e7a82996855c37c4
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-28 18:19:01 -0300

    feat: import and usage CoffeesSection component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { CoffeesSection } from './components/CoffesSection/'
 import { MainBanner } from './components/MainBanner'
 
 export function Home() {
   return (
     <main>
       <MainBanner />
+      <CoffeesSection />
     </main>
   )
 }
