dfca90a2c63bec76699af175d3c8fb867f150923
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-05 09:25:06 -0300

    feat: create Checkout Page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,57 @@
+import { MapPinLine } from 'phosphor-react'
+import {
+  CheckoutSection,
+  CheckoutSectionContainer,
+  AddressContent,
+  Cart,
+  CheckoutContent,
+  AddressContentHeader,
+  CartContainer,
+  ProductList,
+  ProductItem,
+  PurchaseButton,
+} from './styles'
+import { AddressFields } from './components/AddressFields'
+import { PaymentSelect } from './components/PaymentSelect'
+import { CardProductCart } from './components/CardProductCart'
+import { Summary } from './components/Summary'
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <CheckoutSection>
+      <CheckoutSectionContainer>
+        <CheckoutContent>
+          <AddressContent>
+            <h4>Complete seu pedido</h4>
+            <div>
+              <AddressContentHeader>
+                <MapPinLine weight="regular" size={22} />
+                <div>
+                  <h5>Endereço de Entrega</h5>
+                  <p>Informe o endereço onde deseja receber seu pedido</p>
+                </div>
+              </AddressContentHeader>
+              <AddressFields />
+            </div>
+            <PaymentSelect />
+          </AddressContent>
+          <Cart>
+            <h4>Café Selecioandos</h4>
+            <CartContainer>
+              <ProductList>
+                <ProductItem>
+                  <CardProductCart />
+                </ProductItem>
+                <ProductItem>
+                  <CardProductCart />
+                </ProductItem>
+              </ProductList>
+              <Summary />
+              <PurchaseButton type="submit">Confirmar Pedido</PurchaseButton>
+            </CartContainer>
+          </Cart>
+        </CheckoutContent>
+      </CheckoutSectionContainer>
+    </CheckoutSection>
+  )
 }
