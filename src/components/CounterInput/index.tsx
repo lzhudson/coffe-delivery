515cf3d074bd483a80c24d8edddb2dc5fa0473ce
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-09 18:27:00 -0300

    feat(refact): add older functions commented

--- src/components/CounterInput/index.tsx
+++ src/components/CounterInput/index.tsx
@@ -2,43 +2,48 @@ import subtractionIcon from '../../assets/images/subtraction.svg'
 import plusIcon from '../../assets/images/plus.svg'
 import { CounterInputContainer } from './styles'
 
-// interface CounterInputProps {
-//   onIncrementQuantity: () => void
-//   onDecrementQuantity: () => void
-//   onChangeProductQuantity: (quantity: number) => void
-//   quantityProduct: number
-// }
+interface CounterInputProps {
+  onIncrementQuantity: () => void
+  onDecrementQuantity: () => void
+  onChangeProductQuantity: (quantity: number) => void
+  quantityProduct: number
+}
 
-export function CounterInput() {
-  // function handleIncrementQuantity() {
-  //   onIncrementQuantity()
-  // }
+export function CounterInput({
+  onIncrementQuantity,
+  onDecrementQuantity,
+  onChangeProductQuantity,
+  quantityProduct,
+}: CounterInputProps) {
+  function handleIncrementQuantity() {
+    onIncrementQuantity()
+  }
 
-  // function handleDecrementQuantity() {
-  //   onDecrementQuantity()
-  // }
+  function handleDecrementQuantity() {
+    onDecrementQuantity()
+  }
 
-  // const isButtonEnable = quantityProduct <= 1
+  const isButtonEnable = quantityProduct <= 1
   return (
     <CounterInputContainer>
       <button
         type="button"
-        // onClick={handleDecrementQuantity}
-        // disabled={isButtonEnable}
+        onClick={handleDecrementQuantity}
+        disabled={isButtonEnable}
         title="Subtraction Item"
       >
         <img src={subtractionIcon} alt="Subtraction" />
       </button>
       <input
         type="number"
-        // value={quantityProduct}
-        // onChange={(e) => onChangeProductQuantity(Number(e.target.value))}
+        value={quantityProduct}
+        onChange={(e) => onChangeProductQuantity(Number(e.target.value))}
         name="quantityProduct"
         min={1}
       />
       <button
         type="button"
-        // onClick={handleIncrementQuantity}
+        onClick={handleIncrementQuantity}
         title="Adding Item"
       >
         <img src={plusIcon} alt="Subtraction" />
