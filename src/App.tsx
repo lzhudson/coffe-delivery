91d93b8fe07d6a726e258ab8dbcf89941eb48ada
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-09 18:26:44 -0300

    feat: usage CartProvier in every application

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Router } from './Router'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { CartProvider } from './context/CartContext'
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
+    <CartProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <GlobalStyle />
+      </ThemeProvider>
+    </CartProvider>
   )
 }
