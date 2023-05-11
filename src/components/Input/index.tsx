34182328103d4056bd3c694a5d631a4d83d80e31
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-11 16:08:27 -0300

    feat(refact): migrate to Input single file

--- src/components/Input/index.tsx
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes } from 'react'
-import { InputContainer } from './style'
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export function Input({ ...props }: InputProps) {
-  return <InputContainer {...props} />
-}
