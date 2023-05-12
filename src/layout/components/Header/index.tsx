cbe01dbd5492308a15f0f0c876a0726abd5338f3
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-11 23:29:53 -0300

    feat: add link to home and checkout page

--- src/layout/components/Header/index.tsx
+++ src/layout/components/Header/index.tsx
@@ -3,6 +3,7 @@ import logo from '../../../assets/images/logo.svg'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 import { useContext } from 'react'
 import { CartContext } from '../../../context/CartContext'
+import { NavLink } from 'react-router-dom'
 export function Header() {
   const { products } = useContext(CartContext)
   const productQuantity = products.length
@@ -10,16 +11,20 @@ export function Header() {
   return (
     <header>
       <HeaderContainer>
-        <img src={logo} alt="Coffe Delivery" />
+        <NavLink to={'/'}>
+          <img src={logo} alt="Coffe Delivery" />
+        </NavLink>
         <div className="actions-container">
           <LocationBadge>
             <MapPin weight="fill" size={22} />
             Fortaleza, CE
           </LocationBadge>
-          <CartButton>
-            <ShoppingCart weight="fill" size={22} />
-            <span>{productQuantity}</span>
-          </CartButton>
+          <NavLink to={'/checkout'}>
+            <CartButton>
+              <ShoppingCart weight="fill" size={22} />
+              <span>{productQuantity}</span>
+            </CartButton>
+          </NavLink>
         </div>
       </HeaderContainer>
     </header>
