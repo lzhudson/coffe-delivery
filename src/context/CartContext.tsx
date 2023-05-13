4711d8bec8b09ce83f2cce9eba52651a4640dfd5
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-13 15:05:54 -0300

    refact: migrate to reducer pattern

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,18 @@
-import { ReactNode, createContext, useEffect, useState } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
+import { productsReducer } from '../reducers/products/reducers'
+import {
+  addNewProductAction,
+  decreaseQuantityProductAction,
+  increaseQuantityProductAction,
+  removeProductAction,
+  updateQuantityProductAction,
+} from '../reducers/products/actions'
 
 export interface Product {
   id: number
@@ -23,17 +37,19 @@ interface OrderInfo {
   paymentType: 'credit-card' | 'debit-card' | 'money'
 }
 
-interface ProductInput extends Product {
+export interface ProductInput extends Product {
   quantity: number
 }
 
 interface CartContextProps {
   orderInfo: OrderInfo
-  products: Product[]
+  productsState: Product[]
   addProduct: (product: ProductInput) => void
   updateOrderInfo: (orderInfo: OrderInfo) => void
   deleteProduct: (product: number) => void
   updateProductQuantity: (productId: number, quantity: number) => void
+  increaseProductQuantity: (productId: number, quantity: number) => void
+  decreaseProductQuantity: (productId: number, quantity: number) => void
 }
 
 export const CartContext = createContext({} as CartContextProps)
@@ -41,62 +57,40 @@ export const CartContext = createContext({} as CartContextProps)
 interface CartProviderProps {
   children: ReactNode
 }
-
 export function CartProvider({ children }: CartProviderProps) {
-  const [products, setProducts] = useState<Product[]>(() => {
-    const productsInLocalStorage = JSON.parse(
-      localStorage.getItem('@coffee-delivery:products-state-1.0.0') || '',
-    )
-    if (productsInLocalStorage) {
-      return productsInLocalStorage
-    }
-    return []
-  })
-
+  const [productsState, dispatch] = useReducer(
+    productsReducer,
+    [],
+    (initialState) => {
+      const productsInLocalStorage = localStorage?.getItem(
+        '@coffee-delivery:productsState-state-1.0.0',
+      )
+      if (productsInLocalStorage) {
+        return JSON.parse(productsInLocalStorage)
+      }
+      return initialState
+    },
+  )
   const [orderInfo, setOrderInfo] = useState({} as OrderInfo)
 
   function addProduct(product: ProductInput) {
-    const productInput = product
-    const productInCart =
-      products.findIndex((product) => product.id === productInput.id) >= 0
-    if (productInCart) {
-      const productsUpdated = products.map((product) => {
-        if (product.id === productInput.id) {
-          return {
-            ...product,
-            quantity: product.quantity + productInput.quantity,
-          }
-        }
-        return product
-      })
-      setProducts(productsUpdated)
-    } else {
-      setProducts((prevState) => [...prevState, productInput])
-    }
+    dispatch(addNewProductAction(product))
+  }
+
+  function increaseProductQuantity(productId: number) {
+    dispatch(increaseQuantityProductAction(productId))
+  }
+
+  function decreaseProductQuantity(productId: number) {
+    dispatch(decreaseQuantityProductAction(productId))
   }
 
   function updateProductQuantity(productId: number, quantity: number) {
-    const productInCart =
-      products.findIndex((product) => product.id === productId) >= 0
-    if (productInCart) {
-      const productsUpdated = products.map((product) => {
-        if (product.id === productId) {
-          return {
-            ...product,
-            quantity,
-          }
-        }
-        return product
-      })
-      setProducts(productsUpdated)
-    }
+    dispatch(updateQuantityProductAction(productId, quantity))
   }
 
   function deleteProduct(productId: number) {
-    const productsUpdated = products.filter(
-      (product) => product.id !== productId,
-    )
-    setProducts(productsUpdated)
+    dispatch(removeProductAction(productId))
   }
 
   function updateOrderInfo(orderInfo: OrderInfo) {
@@ -104,9 +98,12 @@ export function CartProvider({ children }: CartProviderProps) {
   }
 
   useEffect(() => {
-    const productsJSON = JSON.stringify(products)
-    localStorage.setItem('@coffee-delivery:products-state-1.0.0', productsJSON)
-  }, [products])
+    const productsJSON = JSON.stringify(productsState)
+    localStorage.setItem(
+      '@coffee-delivery:productsState-state-1.0.0',
+      productsJSON,
+    )
+  }, [productsState])
 
   useEffect(() => {
     const orderInfoJSON = JSON.stringify(orderInfo)
@@ -120,9 +117,11 @@ export function CartProvider({ children }: CartProviderProps) {
     <CartContext.Provider
       value={{
         orderInfo,
-        products,
+        productsState,
         addProduct,
         updateProductQuantity,
+        increaseProductQuantity,
+        decreaseProductQuantity,
         updateOrderInfo,
         deleteProduct,
       }}
