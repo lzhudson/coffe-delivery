42c9baacf9625595cb44ded5f95da7d8df08960e
Hudson Holanda <hudsonholanda55@gmail.com>
2023-06-01 13:45:52 -0300

    refact: change styles to InputRoot styled component

--- src/components/FormGroup.ts
+++ src/components/FormGroup.ts
@@ -1,9 +1,3 @@
 import styled from 'styled-components'
 
-export const FormGroup = styled.div`
-  background-color: ${(props) => props.theme.colors['base-input']};
-  border: 1px solid ${(props) => props.theme.colors['base-button']};
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
-`
+export const FormGroup = styled.div``
