f2b7a8c680480a736bd5aaad523a4f3b07a7f4bb
Hudson Holanda <hudsonholanda55@gmail.com>
2023-06-01 13:47:33 -0300

    feat: usage InputRoot and Feedback component

--- src/pages/Checkout/components/AddressFields/index.tsx
+++ src/pages/Checkout/components/AddressFields/index.tsx
@@ -1,46 +1,82 @@
 import { useFormContext } from 'react-hook-form'
 import { FormGroup } from '../../../../components/FormGroup'
 import { FormRow } from '../../../../components/FormRow'
-import { Input } from '../../../../components/Input'
+import { Input, InputRoot } from '../../../../components/Input'
 import { InputGroupText } from '../../../../components/InputGroupText'
 import { AddressFieldContainer } from './style'
 import { AddAddressFormValidationFormData } from '../..'
+import { Feedback } from '../../../../components/Feedback'
 
 export function AddressFields() {
-  const { register } = useFormContext<AddAddressFormValidationFormData>()
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<AddAddressFormValidationFormData>()
   return (
     <AddressFieldContainer>
       <FormRow>
         <FormGroup>
-          <Input placeholder="CEP" {...register('address.zipcode')} />
+          <InputRoot>
+            <Input placeholder="CEP" {...register('address.zipcode')} />
+          </InputRoot>
+          {errors.address?.zipcode?.message && (
+            <Feedback>{errors.address?.zipcode?.message}</Feedback>
+          )}
         </FormGroup>
       </FormRow>
       <FormRow>
         <FormGroup>
-          <Input placeholder="Rua" {...register('address.street')} />
+          <InputRoot>
+            <Input placeholder="Rua" {...register('address.street')} />
+          </InputRoot>
+          {errors.address?.street?.message && (
+            <Feedback>{errors.address?.street?.message}</Feedback>
+          )}
         </FormGroup>
       </FormRow>
       <FormRow>
         <FormGroup>
-          <Input placeholder="Número" {...register('address.number')} />
+          <InputRoot>
+            <Input placeholder="Número" {...register('address.number')} />
+          </InputRoot>
+          {errors.address?.number?.message && (
+            <Feedback>{errors.address?.number?.message}</Feedback>
+          )}
         </FormGroup>
         <FormGroup>
-          <Input
-            placeholder="Complemento"
-            {...register('address.complement')}
-          />
-          <InputGroupText>Opcional</InputGroupText>
+          <InputRoot>
+            <Input
+              placeholder="Complemento"
+              {...register('address.complement')}
+            />
+            <InputGroupText>Opcional</InputGroupText>
+          </InputRoot>
         </FormGroup>
       </FormRow>
       <FormRow>
         <FormGroup>
-          <Input placeholder="Bairro" {...register('address.neighborhood')} />
+          <InputRoot>
+            <Input placeholder="Bairro" {...register('address.neighborhood')} />
+          </InputRoot>
+          {errors.address?.neighborhood?.message && (
+            <Feedback>{errors.address?.neighborhood?.message}</Feedback>
+          )}
         </FormGroup>
         <FormGroup>
-          <Input placeholder="Cidade" {...register('address.city')} />
+          <InputRoot>
+            <Input placeholder="Cidade" {...register('address.city')} />
+          </InputRoot>
+          {errors.address?.city?.message && (
+            <Feedback>{errors.address?.city?.message}</Feedback>
+          )}
         </FormGroup>
         <FormGroup>
-          <Input placeholder="UF" {...register('address.federativeUnity')} />
+          <InputRoot>
+            <Input placeholder="UF" {...register('address.federativeUnity')} />
+          </InputRoot>
+          {errors.address?.federativeUnity?.message && (
+            <Feedback>{errors.address?.federativeUnity?.message}</Feedback>
+          )}
         </FormGroup>
       </FormRow>
     </AddressFieldContainer>
