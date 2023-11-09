38d5c406a337b8090fcb141aba2dfbdc440b3a7f
Hudson Holanda <hudsonholanda55@gmail.com>
2023-11-09 16:01:10 -0300

    chore: add base if environtment not equal development

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://lzhudson.github.io/coffee-delivery',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    ...(env.NODE_ENV !== 'development' && { base: '/cofee-delivery' }),
+  }
 })
